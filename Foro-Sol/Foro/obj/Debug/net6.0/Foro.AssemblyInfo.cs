e1ee10e6cf5c645109a7c330fa186bf85a4824b5
Débora Landa <debylanda02@gmail.com>
2024-05-19 11:46:39 -0300

    costumizado

--- Foro-Sol/Foro/obj/Debug/net6.0/Foro.AssemblyInfo.cs
+++ Foro-Sol/Foro/obj/Debug/net6.0/Foro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9bdb000294cc6c8f8fd81dc6e77d84186daca8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649373cc05eae555f87fc0c4816e870e9d2e270d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
