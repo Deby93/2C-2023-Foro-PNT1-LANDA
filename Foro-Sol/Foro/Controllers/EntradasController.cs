979ffbd87e0f232f9a7e2feebc71ae78965498f4
Débora Landa <debylanda02@gmail.com>
2024-06-04 14:23:40 -0300

    Puede preguntar si esta autenticado

--- Foro-Sol/Foro/Controllers/EntradasController.cs
+++ Foro-Sol/Foro/Controllers/EntradasController.cs
@@ -21,22 +21,7 @@ namespace Foro
             _signinManager = signinManager;
 
         }
-        // GET: Entradas
-        //public async Task<IActionResult> Index()
-        //{
-        //    List<Entrada> listaDeEntradas = new();
-
-        //    listaDeEntradas = await _contexto.Entradas.
-        //       Include(e => e.Categoria).
-        //       Include(e => e.Miembro).
-        //       Include(e => e.MiembrosHabilitados).
-        //       OrderByDescending(e => e.Fecha).
-        //       ToListAsync();
-
-        //    return View(listaDeEntradas);
-        //}
-
-
+       
         public ActionResult Index()
         {
             // Obtener la entrada con más dislikes
--- Foro-Sol/Foro/obj/Debug/net6.0/Foro.AssemblyInfo.cs
+++ Foro-Sol/Foro/obj/Debug/net6.0/Foro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce9e36ebffd4f5984944aa35349b0f07b9cd3f33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95617c028a15a383c552ec27d7f512ad1b9e76f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
